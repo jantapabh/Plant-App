c9e6a28a2a26638bc542ab80673c9243969cf458
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-13 16:20:21 +0700

    add commit

--- lib/screen/components/body.dart
+++ lib/screen/components/body.dart
@@ -20,14 +20,25 @@ class Body extends StatelessWidget {
                         bottomLeft: Radius.circular(36),
                         bottomRight: Radius.circular(36))),
               ),
-              Positioned(child:
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child:
                Container(
                 margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
                 height: 54,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(20),
+                  boxShadow: [BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23)
+                  ), ]
                 ),
+                child: TextField(),
               ),
               ),
             ],
