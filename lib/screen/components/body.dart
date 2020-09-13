7d09305124a2a1f1f74a698b3605eaaaf0382a5d
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-13 16:52:09 +0700

    add commit

--- lib/screen/components/body.dart
+++ lib/screen/components/body.dart
@@ -25,33 +25,34 @@ class Body extends StatelessWidget {
                 bottom: 0,
                 left: 0,
                 right: 0,
-                child:
-               Container(
-                 alignment: Alignment.center,
-                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                height: 54,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23)
-                  ), ]
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 50,
+                            color: kPrimaryColor.withOpacity(0.23)),
+                      ]),
+                  child: Row(children: <Widget>[
+                    Expanded(child:  TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: kPrimaryColor.withOpacity(0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
+                      ),
+                    ),)
+                  ]),
                 ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search",
-                    hintStyle: TextStyle(
-                      color: kPrimaryColor.withOpacity(0.5),
-                    ),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    suffixIcon: SvgPicture.asset("assets/icons/search.svg")
-                  ),
-                ),
-              ),
               ),
             ],
           ),
