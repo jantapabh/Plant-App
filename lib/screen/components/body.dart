9e82e4c73e46476794aaa121d4b83aa6d731dd17
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-13 16:10:00 +0700

    add commit

--- lib/screen/components/body.dart
+++ lib/screen/components/body.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     //It will Provide us total height and width of our screen
     Size size = MediaQuery.of(context).size;
     return Column(
       children: <Widget>[
         Container(
           height: size.height * 0.2,
-          color: Colors.black,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: size.height * 0.2 - 27,
+                decoration: BoxDecoration(
+                    color: kPrimaryColor,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(36),
+                        bottomRight: Radius.circular(36))),
+              ),
+              Positioned(child:
+               Container(
+                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                height: 54,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              ),
+            ],
+          ),
         )
       ],
     );
