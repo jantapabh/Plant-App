5812e579931d662b75cf93267db61ea6c600e98c
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-13 13:20:36 +0700

    add commit svg image

--- lib/components/home_screen.dart
+++ lib/components/home_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 
 class Homescreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container(
-
+    return Scaffold(
+      appBar: AppBar(
+        elevation:0,
+        leading: IconButton(icon: SvgPicture.asset("assetName"))
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
         primaryColor: kPrimaryColor,
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
       ),
