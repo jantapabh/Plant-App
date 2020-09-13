224676f28e18e2f3221f12a469aefd8d03ed6fc4
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-13 15:52:18 +0700

    add commit

--- lib/components/home_screen.dart
+++ lib/components/home_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-
-class Homescreen extends StatelessWidget{
+class Homescreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation:0,
-        leading: IconButton(icon: SvgPicture.asset("assets/icons/menu.svg"),
-        onPressed: () {
-          
-        },)
-      ),
+      appBar: buildAppBar(),
     );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+          onPressed: () {},
+        ));
   }
 }
